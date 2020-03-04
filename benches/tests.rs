e53be787f56bff715d6bd924f4ee2bb648fbaf61
Egor Ivkov <e.o.ivkov@gmail.com>
2020-03-04 16:59:59 +0300

    struct::new instead of get_struct
    also improved readability of imports

--- benches/tests.rs
+++ benches/tests.rs
@@ -3,26 +3,9 @@ extern crate bencher;
 
 use bencher::Bencher;
 use onecode::ser as one_code_ser;
-use parity_scale_codec::Encode;
+use parity_scale_codec;
 
-pub fn get_test_struct() -> StructToSerialize{
-    let struct_to_serialize = StructToSerialize {
-        boolean: true,
-        positive_integer: 1,
-        negative_integer: -1,
-        empty_string: "".to_string(),
-        number_string: "0.1".to_string(),
-        latin_string: "hello world".to_string(),
-        cyrillic_string: "привет мир".to_string(),
-        japan_string: "こんにちは世界".to_string(),
-        number_list: vec![1, 2, 3],
-        string_list: vec!["1".to_string(), "01".to_string(), "011".to_string()],
-        null: Option::None,
-    };
-    struct_to_serialize
-}
-
-#[derive(serde::Serialize, Debug, PartialEq, Encode)]
+#[derive(serde::Serialize, Debug, PartialEq, parity_scale_codec::Encode)]
 pub struct StructToSerialize {
     boolean: bool,
     positive_integer: u8,
@@ -37,22 +20,40 @@ pub struct StructToSerialize {
     null: Option<()>,
 }
 
+impl StructToSerialize {
+    pub fn new() -> Self {
+        StructToSerialize {
+            boolean: true,
+            positive_integer: 1,
+            negative_integer: -1,
+            empty_string: "".to_string(),
+            number_string: "0.1".to_string(),
+            latin_string: "hello world".to_string(),
+            cyrillic_string: "привет мир".to_string(),
+            japan_string: "こんにちは世界".to_string(),
+            number_list: vec![1, 2, 3],
+            string_list: vec!["1".to_string(), "01".to_string(), "011".to_string()],
+            null: Option::None,
+        }
+    }
+}
+
 fn serialize_1code(bench: &mut Bencher) {
-    let struct_to_serialize = get_test_struct();
+    let struct_to_serialize = StructToSerialize::new();
     bench.iter(|| {
         one_code_ser::to_string(&struct_to_serialize).unwrap();
     });
 }
 
 fn serialize_scale(bench: &mut Bencher) {
-    let struct_to_serialize = get_test_struct();
+    let struct_to_serialize = StructToSerialize::new();
     bench.iter(|| {
-        struct_to_serialize.encode();
+        parity_scale_codec::Encode::encode(&struct_to_serialize);
     });
 }
 
 fn serialize_bincode(bench: &mut Bencher) {
-    let struct_to_serialize = get_test_struct();
+    let struct_to_serialize = StructToSerialize::new();
     bench.iter(|| {
         bincode::serialize(&struct_to_serialize).unwrap();
     });
--- tests/size_cmp.rs
+++ tests/size_cmp.rs
@@ -1,24 +1,7 @@
-use parity_scale_codec::Encode;
+use parity_scale_codec;
 use onecode::ser as one_code_ser;
 
-pub fn get_test_struct() -> StructToSerialize{
-    let struct_to_serialize = StructToSerialize {
-        boolean: true,
-        positive_integer: 1,
-        negative_integer: -1,
-        empty_string: "".to_string(),
-        number_string: "0.1".to_string(),
-        latin_string: "hello world".to_string(),
-        cyrillic_string: "привет мир".to_string(),
-        japan_string: "こんにちは世界".to_string(),
-        number_list: vec![1, 2, 3],
-        string_list: vec!["1".to_string(), "01".to_string(), "011".to_string()],
-        null: Option::None,
-    };
-    struct_to_serialize
-}
-
-#[derive(serde::Serialize, Debug, PartialEq, Encode)]
+#[derive(serde::Serialize, Debug, PartialEq, parity_scale_codec::Encode)]
 pub struct StructToSerialize {
     boolean: bool,
     positive_integer: u8,
@@ -33,23 +16,41 @@ pub struct StructToSerialize {
     null: Option<()>,
 }
 
+impl StructToSerialize {
+    pub fn new() -> Self {
+        StructToSerialize {
+            boolean: true,
+            positive_integer: 1,
+            negative_integer: -1,
+            empty_string: "".to_string(),
+            number_string: "0.1".to_string(),
+            latin_string: "hello world".to_string(),
+            cyrillic_string: "привет мир".to_string(),
+            japan_string: "こんにちは世界".to_string(),
+            number_list: vec![1, 2, 3],
+            string_list: vec!["1".to_string(), "01".to_string(), "011".to_string()],
+            null: Option::None,
+        }
+    }
+}
+
 #[test]
 fn size_1code(){
-    let struct_to_serialize = get_test_struct();
+    let struct_to_serialize = StructToSerialize::new();
     let encoded = one_code_ser::to_string(&struct_to_serialize).unwrap();
     println!("1code size: {} bytes", encoded.as_bytes().len());
 }
 
 #[test]
 fn size_scale() {
-    let struct_to_serialize = get_test_struct();
-    let encoded = struct_to_serialize.encode();
+    let struct_to_serialize = StructToSerialize::new();
+    let encoded = parity_scale_codec::Encode::encode(&struct_to_serialize);
     println!("scale size: {} bytes", encoded.len());
 }
 
 #[test]
 fn size_bincode() {
-    let struct_to_serialize = get_test_struct();
+    let struct_to_serialize = StructToSerialize::new();
     let encoded = bincode::serialize(&struct_to_serialize).unwrap();
     println!("bincode size: {} bytes", encoded.len());
 }
